6d8cef394402a9b76d7ea458de5e825a64ca704b
Marta Kowalik <63840553+palacz56@users.noreply.github.com>
2020-11-06 23:21:13 +0100

    Update orgpracownicy.php

--- orgpracownicy.php
+++ orgpracownicy.php
@@ -3,18 +3,18 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Karolina Zatylny 2Ti</title>
+        <title>Marcia Kowalik</title>
         <link rel="stylesheet" href="styles.css">
     </head>
 <body>
 
 <h3>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-karolinazatylny"><b>GITHUB | </b></a>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Marta-Kowalik"><b>GITHUB | </b></a>
     <a href="agregat.php"><b>FUNKCJE AGREGUJĄCE | </b></a>
     <a href="orgpracownicy.php"><b>ORGANIZACJA I PRACOWNICY | </b></a>
     <a href="pracownicy.php"><b>PRACOWNICY</b></a>
 </h3>
-<h1>✿ Karolina Zatylny ✿</h1>
+<h1>Marta Kowalik</h1>
 <hr>
 
 <?php
