436d864185cf0d59806fc664d86b171a7595191b
Marta Kowalik <63840553+palacz56@users.noreply.github.com>
2020-11-20 14:42:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <a href="pracownicy.php"><b>PRACOWNICY | </b></a>
     <a href="dataiczas.php"><b>DATA I CZAS | </b></a> 
     <a href="formularz.html"><b>FORMULARZ | </b></a>
-    <a href="insert.php"><b>INSERT | </b></a>
+    
     
 </h3>
 </div>
