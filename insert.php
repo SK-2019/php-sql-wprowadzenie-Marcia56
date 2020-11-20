c93daa9dff1ffa9846c122cb4f23ad42a2f3898e
Marta Kowalik <63840553+palacz56@users.noreply.github.com>
2020-11-20 14:36:55 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,14 +1,27 @@
 <?php
+echo("jestes w insert.php");
+echo $_POST['name'];
 
-require("connect.php");
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
 
-$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki) 
-       VALUES (null,'Balbina', 4, 86,'1999-05-21')";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Pracownik (null,imie, dzial, zarobki) 
+       VALUES (null,".$_POST['name'].", 1, 76,'1991-11-21')";
+
+echo "<li>".$sql;
+
+
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
-//informacja o ewentualnych błędach
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
