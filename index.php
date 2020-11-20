cadd5cff93c0c9df61219ef41c223398689985c1
Marta Kowalik <63840553+palacz56@users.noreply.github.com>
2020-11-20 14:21:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <a href="agregat.php"><b>FUNKCJE AGREGUJĄCE | </b></a>
     <a href="orgpracownicy.php"><b>ORGANIZACJA I PRACOWNICY | </b></a>
     <a href="pracownicy.php"><b>PRACOWNICY | </b></a>
-    <a href="dataiczas.php"><b>DATA I CZAS</b></a> 
+    <a href="dataiczas.php"><b>DATA I CZAS | </b></a> 
     <a href="formularz.html"><b>FORMULARZ | </b></a>
 </h3>
 </div>
