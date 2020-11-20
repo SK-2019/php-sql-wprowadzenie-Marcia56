59476cba8abea05bc5b0d013ee2a0dd42c232e15
Marta Kowalik <63840553+palacz56@users.noreply.github.com>
2020-11-20 14:37:26 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,6 +1,9 @@
 <?php
-echo("jestes w insert.php");
-echo $_POST['name'];
+echo("jestes w insert.php <br>");
+echo "<li>". $_POST['name'];
+echo "<li>". $_POST['dzial'];
+echo "<li>". $_POST['zarobki'];
+echo "<li>". $_POST['data_urodzenia'];
 
 $servername = "localhost";
 $username = "username";
@@ -12,12 +15,18 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO Pracownik (null,imie, dzial, zarobki) 
-       VALUES (null,".$_POST['name'].", 1, 76,'1991-11-21')";
+//definiujemy zapytanie $sql
+$sql = "INSERT INTO Pracownik (null, name, dzial,zarobki,data_urodzenia)
+	      VALUES (
+					null,". 
+					$_POST['name'], 
+					$_POST['dzial'], 
+					$_POST['zarobki'],
+					$_POST['data_urodzenia']
+				;
 
-echo "<li>".$sql;
-
-
+//wyświetlamy zapytanie $sql
+echo "<li>". $sql;
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
