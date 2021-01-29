b7b6f3aaa71522ce562f8bbd42a7e925ab77ed0d
Marta Kowalik <63840553+palacz56@users.noreply.github.com>
2021-01-29 09:14:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
           
         <div class="tresc">
           
-          <div class="costam"> aaaaaaaaa</div>
+          <div class="costam"> Strona główna</div>
           <?php
 
           require("connect.php");
