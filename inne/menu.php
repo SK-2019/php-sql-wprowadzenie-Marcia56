ed3d6ea04caf2fc6968477a1e4ed8be433cbd20f
Marta Kowalik <63840553+palacz56@users.noreply.github.com>
2021-04-21 21:57:46 +0200

    Create menu.php

---
+++ inne/menu.php
@@ -0,0 +1,9 @@
+          <a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-Marta-Kowalik">GITHUB</a>
+          <a class="link" href="/index.php">INDEX</a>
+          <a class="link" href="../pracownicy/agregat.php"><b>FUNKCJE AGREGUJĄCE</b></a>
+          <a class="link" href="../pracownicy/orgpracownicy.php">ORGANIZACJA I PRACOWNICY</a>
+          <a class="link" href="../pracownicy/pracownicy.php">PRACOWNICY</a>
+          <a class="link" href="../pracownicy/dataiczas.php">DATA I CZAS</a>
+          <a class="link" href="../formularz/formularz.html">FORMULARZ</a>
+          <a class="link" href="../biblioteka/ksiazki.html">KSIAZKI</a>
+          <a class="link" href="flexbox/flexbox.html">FLEXBOX</a>
