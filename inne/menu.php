d5b69effe238e8d97f62402bffae607623cd0108
Marta Kowalik <63840553+palacz56@users.noreply.github.com>
2021-04-22 03:49:05 +0200

    Update menu.php

--- inne/menu.php
+++ inne/menu.php
@@ -1,10 +1,10 @@
-          <a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-Marta-Kowalik">GITHUB<img class="obrazek" src="../zdjecia/hk1.gif"></a>
-          <a class="link" href="/index.php">INDEX<img class="obrazek" src="../zdjecia/hk3.gif"></a>
-          <a class="link" href="../pracownicy/agregat.php">FUNKCJE AGREGUJĄCE<img class="obrazek" src="../zdjecia/hk3.gif"></a>
-          <a class="link" href="../pracownicy/orgpracownicy.php">PRACOWNICY/ORGANIZACJA<img class="obrazek" src="../zdjecia/hk1.gif"></a>
-          <a class="link" href="../pracownicy/pracownicy.php">PRACOWNICY<img class="obrazek" src="../zdjecia/hk3.gif"></a>
-          <a class="link" href="../pracownicy/dataiczas.php">DATA I CZAS<img class="obrazek" src="../zdjecia/hk1.gif"></a>
-          <a class="link" href="../formularz/formularz.html">FORMULARZ<img class="obrazek" src="../zdjecia/hk3.gif"></a>
-          <a class="link" href="../biblioteka/ksiazki.php">KSIAZKI<img class="obrazek" src="../zdjecia/hk1.gif"></a>
-          <a class="link" href="../flexbox/flexbox.php">FLEXBOX<img class="obrazek" src="../zdjecia/hk3.gif"></a>
-          <a class="link" href="https://www.notion.so/SQL-3ccfadeab4b84d7794d7a13966de2547">NOTION<img class="obrazek" src="../zdjecia/hk1.gif"></a>
+          <a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-Marta-Kowalik">GITHUB<img class="obrazek" src="../obrazki/hk1.gif"></a>
+          <a class="link" href="/index.php">INDEX<img class="obrazek" src="../obrazki/hk3.gif"></a>
+          <a class="link" href="../pracownicy/agregat.php">FUNKCJE AGREGUJĄCE<img class="obrazek" src="../obrazki/hk3.gif"></a>
+          <a class="link" href="../pracownicy/orgpracownicy.php">PRACOWNICY/ORGANIZACJA<img class="obrazek" src="../obrazki/hk1.gif"></a>
+          <a class="link" href="../pracownicy/pracownicy.php">PRACOWNICY<img class="obrazek" src="../obrazki/hk3.gif"></a>
+          <a class="link" href="../pracownicy/dataiczas.php">DATA I CZAS<img class="obrazek" src="../obrazki/hk1.gif"></a>
+          <a class="link" href="../formularz/formularz.html">FORMULARZ<img class="obrazek" src="../obrazki/hk3.gif"></a>
+          <a class="link" href="../biblioteka/ksiazki.php">KSIAZKI<img class="obrazek" src="../obrazki/hk1.gif"></a>
+          <a class="link" href="../flexbox/flexbox.php">FLEXBOX<img class="obrazek" src="../obrazki/hk3.gif"></a>
+          <a class="link" href="https://www.notion.so/SQL-3ccfadeab4b84d7794d7a13966de2547">NOTION<img class="obrazek" src="../obrazki/hk1.gif"></a>
