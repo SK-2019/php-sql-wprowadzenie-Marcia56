7fb8112cc533899d296d19a2404a8e6424a6f82d
Marta Kowalik <63840553+palacz56@users.noreply.github.com>
2021-04-22 03:24:17 +0200

    Update menu.php

--- inne/menu.php
+++ inne/menu.php
@@ -1,10 +1,10 @@
-          <a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-Marta-Kowalik">GITHUB</a>
-          <a class="link" href="/index.php">INDEX</a>
-          <a class="link" href="../pracownicy/agregat.php"><b>FUNKCJE AGREGUJĄCE</b></a>
-          <a class="link" href="../pracownicy/orgpracownicy.php">ORGANIZACJA I PRACOWNICY</a>
-          <a class="link" href="../pracownicy/pracownicy.php">PRACOWNICY</a>
-          <a class="link" href="../pracownicy/dataiczas.php">DATA I CZAS</a>
-          <a class="link" href="../formularz/formularz.html">FORMULARZ</a>
-          <a class="link" href="../biblioteka/ksiazki.php">KSIAZKI</a>
-          <a class="link" href="../flexbox/flexbox.php">FLEXBOX</a>
-          <a class="link" href="https://www.notion.so/SQL-3ccfadeab4b84d7794d7a13966de2547">NOTION</a>
+          <a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-Marta-Kowalik"><img class="obrazek" src="../hk1.gif">GITHUB</a>
+          <a class="link" href="/index.php">INDEX<img class="obrazek" src="../hk1.gif"></a>
+          <a class="link" href="../pracownicy/agregat.php">FUNKCJE AGREGUJĄCE<img class="obrazek" src="../hk1.gif"></a>
+          <a class="link" href="../pracownicy/orgpracownicy.php">ORGANIZACJA I PRACOWNICY<img class="obrazek" src="../hk1.gif"></a>
+          <a class="link" href="../pracownicy/pracownicy.php">PRACOWNICY<img class="obrazek" src="../hk1.gif"></a>
+          <a class="link" href="../pracownicy/dataiczas.php">DATA I CZAS<img class="obrazek" src="../hk1.gif"></a>
+          <a class="link" href="../formularz/formularz.html">FORMULARZ<img class="obrazek" src="../hk1.gif"></a>
+          <a class="link" href="../biblioteka/ksiazki.php">KSIAZKI<img class="obrazek" src="../hk1.gif"></a>
+          <a class="link" href="../flexbox/flexbox.php">FLEXBOX<img class="obrazek" src="../hk1.gif"></a>
+          <a class="link" href="https://www.notion.so/SQL-3ccfadeab4b84d7794d7a13966de2547">NOTION<img class="obrazek" src="../hk1.gif"></a>
