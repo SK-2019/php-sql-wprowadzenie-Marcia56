a511b5a66084766b157caa219cc52e24b05a1e4e
Marta Kowalik <63840553+palacz56@users.noreply.github.com>
2021-05-17 10:41:03 +0200

    Update delete.php

--- relacje/delete.php
+++ relacje/delete.php
@@ -2,12 +2,11 @@
         require_once("../connect.php");
 
         function delete($conn, $table, $column, $row){
-            $sql = "DELETE  FROM $table WHERE $column = $row";
-            
+            $sql = "DELETE  FROM $table WHERE $column = $row";            
             echo $sql;
 
             if ($conn->query($sql) == TRUE) {
-            echo ("<h2>Record deleted successfully</h2>");
+            echo ("<h2>Usunięto</h2>");
             } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
             }
